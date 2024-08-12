62a838f7b36b740c6afc600e375292b747fe453b
1TB <florencgmz@gmail.com>
2024-08-12 05:15:04 -0300

    Fix expanded accordion height

--- src/components/Patient/PatientItem.tsx
+++ src/components/Patient/PatientItem.tsx
@@ -72,7 +72,7 @@ const PatientItem: React.FC<PatientItemProps> = ({
       </button>
       <div
         className={`overflow-hidden transition-all duration-500 ease-in-out ${
-          isExpanded ? 'max-h-[500px] opacity-100' : 'max-h-0 opacity-0'
+          isExpanded ? 'max-h-[700px] opacity-100' : 'max-h-0 opacity-0'
         }`}
       >
         <div className="p-4">
