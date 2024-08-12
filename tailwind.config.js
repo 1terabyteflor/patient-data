963aa630678e6605b10d344f1a881ce582817cdb
1TB <florencgmz@gmail.com>
2024-08-12 04:54:56 -0300

    Modified styles and tailwind config for extended color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-dark": "#063248",
+        "secondary-cyan": "#E3F1F4",
+        "third-beige": "#F8F8F7"
+      },
+    },
   },
   plugins: [],
 }
