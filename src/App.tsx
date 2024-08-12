c2036a962b03ff0ea7fcfeaa4d413f976e5f8a2d
1TB <florencgmz@gmail.com>
2024-08-12 04:56:36 -0300

    Minimal changes on App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import PatientRecords from './components/Patient';
+import Patient from './components/Patient/PatientView';
 
 function App() {
   return (
-    <div className="bg-[#F9F9F9] px-4">
-      <h1 className="text-3xl lg:text-8xl font-bold uppercase tracking-tighter text-gray-700">
+    <div className="bg-third-beige p-4">
+      <h1 className="text-3xl lg:text-8xl font-bold uppercase text-primary-dark mb-4">
         Patient Data Management
       </h1>
-      <PatientRecords />
+      <Patient />
     </div>
   );
 }
